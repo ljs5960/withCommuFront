8c3fde86e04472c861d66390183886a691d1d0ec
김도영 <zz01064842675@gmail.com>
2023-01-04 16:39:06 +0900

    서버 포트

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
+
+const isLocal = process.env.NODE_ENV === "local";
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === "production";
+const ENV = process.env.NODE_ENV
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  trailingSlash: true,
+  serverRuntimeConfig: { // Will only be available on the server side
+    rootDir: path.join(__dirname, './'),
+    hostname: 'localhost',
+    PORT: 8101,
+    HTTP: 'http'
+  },
+  publicRuntimeConfig: { // Will be available on both server and client
+    staticFolder: '/public',
+    isDev,
+    isLocal,
+    isProd,
+    ENV,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
