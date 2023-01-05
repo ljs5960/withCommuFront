db1a90b8707951ef3353fd2ca25402ea91cfd024
김도영 <zz01064842675@gmail.com>
2023-01-05 10:15:45 +0900

    tailwind init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-import ToastUtils from "../utils/ToastUtile";
 import SSRProvider from 'react-bootstrap/SSRProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import wrapper2 from "../redux/store/store";
 import {Provider} from "react-redux";
+import ToastUtils from "@utils/ToastUtile";
 
 function App({Component, pageProps}: AppProps) {
   let {store, props} = wrapper2.useWrappedStore(pageProps);
