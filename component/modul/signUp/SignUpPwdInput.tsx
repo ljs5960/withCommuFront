25b7bbfc647dc659cec5650b95a06485f3c047f6
김도영 <zz01064842675@gmail.com>
2023-01-06 15:22:39 +0900

    prettier 사용

--- component/modul/signUp/SignUpPwdInput.tsx
+++ component/modul/signUp/SignUpPwdInput.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import {UseFormRegisterReturn} from "react-hook-form";
+import React from 'react'
+import { UseFormRegisterReturn } from 'react-hook-form'
 
 type PropsType = {
-    register: UseFormRegisterReturn
+	register: UseFormRegisterReturn
 }
 
 const SignUpPwdInput = (props: PropsType) => {
-    return (
-        <>
-            <div className="col-span-6 sm:col-span-6">
-                <label htmlFor="password"
-                       className="block text-sm font-medium text-gray-700">
-                    password
-                </label>
-                <input
-                    type="text"
-                    id="password"
-                    autoComplete="off"
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    {...props.register}
-                />
-            </div>
-        </>
-    );
-};
+	return (
+		<>
+			<div className="col-span-6 sm:col-span-6">
+				<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+					password
+				</label>
+				<input
+					type="text"
+					id="password"
+					autoComplete="off"
+					className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+					{...props.register}
+				/>
+			</div>
+		</>
+	)
+}
 
-export default SignUpPwdInput;
+export default SignUpPwdInput
--- utils/ToastUtile.tsx
+++ utils/ToastUtile.tsx
@@ -1,38 +1,32 @@
-import {useDispatch, useSelector} from "react-redux";
-import {Toast} from "../redux/types/toastType";
-import {SetStateAction, useEffect, useRef, useState} from "react";
-import {AppStore, RootState} from "../redux/store/store"
-import {toastActions} from "@redux/reducers/toast";
-import ToastPop from "@component/modul/toast/toastPop";
-
+import { useDispatch, useSelector } from 'react-redux'
+import { Toast } from '../redux/types/toastType'
+import { SetStateAction, useEffect, useRef } from 'react'
+import { RootState } from '../redux/store/store'
+import { toastActions } from '@redux/reducers/toast'
+import ToastPop from '@component/modul/toast/toastPop'
 
 const ToastUtils = () => {
-    let dispatch = useDispatch();
-    const toast:Toast[] = useSelector((state: RootState) => state.toast);
+	let dispatch = useDispatch()
+	const toast: Toast[] = useSelector((state: RootState) => state.toast)
 
-    const fire = useRef<SetStateAction<any>>();
+	const fire = useRef<SetStateAction<any>>()
 
-    const timeOut = () => {
-        fire.current = setTimeout(() => {
-            dispatch(toastActions.deleteToast());
-        }, 1500)
-    }
+	const timeOut = () => {
+		fire.current = setTimeout(() => {
+			dispatch(toastActions.deleteToast())
+		}, 1500)
+	}
 
-    useEffect(() => {
-        if (toast.length > 0) {
-            timeOut();
-        }
-        return ()=>{
-            clearTimeout(fire.current);
-        }
-    }, [toast]);
+	useEffect(() => {
+		if (toast.length > 0) {
+			timeOut()
+		}
+		return () => {
+			clearTimeout(fire.current)
+		}
+	}, [toast])
 
-    return (
-        <>
-            { toast.length > 0 && <ToastPop msg={toast[0].msg}></ToastPop> }
-            <ToastPop msg={"123"}></ToastPop>
-        </>
-    )
+	return <>{toast.length > 0 && <ToastPop msg={toast[0].msg}></ToastPop>}</>
 }
 
-export default ToastUtils;
+export default ToastUtils
